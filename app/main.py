3bc709ca945a36033cc503b87cade2f3f9581ca4
losdan77 <losdan322@gmail.com>
2024-05-05 23:07:58 +0300

    first theme (simple api)

---
+++ app/main.py
@@ -0,0 +1,43 @@
+#uvicorn main:app --reload
+from fastapi import FastAPI, Query, Depends
+from pydantic import BaseModel
+from datetime import date
+
+
+app = FastAPI(title='Tranding App')
+
+
+@app.get('/')
+def hello() -> str:
+    return 'hi wirld'
+
+class Hotels_args():
+    def __init__(self,
+        hotel_area: str, 
+        date_start: date, 
+        date_end: date,
+        spa: bool = Query(None), 
+        stars: int = Query(None, ge=1, le=5),):
+
+        self.hotel_area = hotel_area
+        self.date_start = date_start
+        self.date_end = date_end 
+        self.spa = spa
+        self.stars = stars
+
+
+@app.get('/hotels/{hotel_area}')
+def hotels(search_args: Hotels_args = Depends()):
+    
+    return search_args
+
+class SBooking(BaseModel):
+    room_id: int
+    date_start: date
+    date_end: date
+
+
+@app.post('/booking')
+def booking(booking: SBooking) -> str:
+    
+    return 'seccesfull'
