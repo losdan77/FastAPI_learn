2579631d2b46b11974c80df96c82d00068dca9fd
losdan77 <losdan322@gmail.com>
2024-06-12 15:16:51 +0300

    delete test_db for deploy

--- app/config.py
+++ app/config.py
@@ -23,15 +23,15 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
     
-    TEST_DB_HOST: str
-    TEST_DB_PORT: int
-    TEST_DB_USER: str
-    TEST_DB_PASS: str
-    TEST_DB_NAME: str
+    # TEST_DB_HOST: str
+    # TEST_DB_PORT: int
+    # TEST_DB_USER: str
+    # TEST_DB_PASS: str
+    # TEST_DB_NAME: str
     
-    @property
-    def TEST_DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+    # @property
+    # def TEST_DATABASE_URL(self):
+    #     return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
 
     SMTP_HOST: str
